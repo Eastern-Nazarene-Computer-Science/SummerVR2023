3c61950d955cd37e764ceb43c7dfad4461f3e50b
cassied42 <david.c.cassie@lions.enc.edu>
2023-06-26 13:27:33 -0400

    fix?

---
+++ Assets/Scripts/TextDisappear.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class TextDisappear : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh;
+    public float delaytime;
+    public TextMeshProUGUI textMesh2;
+    private void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+
+        StartCoroutine(DisappearAfterDelay(delaytime));
+    }
+
+    private IEnumerator DisappearAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        textMesh.enabled = false;
+        textMesh2.enabled = true;
+    }
+
+
+}
