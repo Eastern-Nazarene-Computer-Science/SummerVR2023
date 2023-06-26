c2704085332dc399b7e320b12dcaf766eff8e84d
lily1c <125397761+lily1c@users.noreply.github.com>
2023-06-26 12:52:16 -0400

    Added changes to Paradise room

---
+++ Assets/ResetGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+    public string initialSceneName = "InitialScene"; // Name of the initial scene
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(initialSceneName);
+    }
+}
