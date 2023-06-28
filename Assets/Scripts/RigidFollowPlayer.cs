03070fd43ac4502fb57afed8dcbe78967d8b0b2c
smithincqhs <81047256+smithincqhs@users.noreply.github.com>
2023-06-28 13:49:49 -0400

    Quicksave

--- Assets/Scripts/RigidFollowPlayer.cs
+++ Assets/Scripts/RigidFollowPlayer.cs
@@ -2,27 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class RigidFollowPlayer : MonoBehaviour
 {
     CapsuleCollider capsule;
     Rigidbody playerRB;
     float playerHeight;
-    public Camera mainCamera;
+    public GameObject mainCamera;
     public XROrigin xrbruh;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRB = gameObject.GetComponent<Rigidbody>();
         capsule = gameObject.GetComponent<CapsuleCollider>();
-        playerHeight = xrbruh.CameraInOriginSpaceHeight;
+        playerHeight = mainCamera.transform.position.y;
         capsule.height = playerHeight;
-        capsule.center = new Vector3(0, playerHeight / 2, 0);
+        capsule.center = new Vector3(mainCamera.transform.localPosition.x, -playerHeight + 0.5f, mainCamera.transform.localPosition.z);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        capsule.center = new Vector3(mainCamera.transform.localPosition.x, playerHeight / 2, mainCamera.transform.localPosition.z);
-    }
 }
